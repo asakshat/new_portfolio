2db9b5762b21b387d9ec7eb7b6ee81727debec6b
asakshat <asakshat453@gmail.com>
2024-06-11 09:19:29 +0200

    added github link

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -9,21 +9,20 @@ export default function Body () {
     const currentPhrase = phrases[currentPhraseIndex];
   
     if (displayedText !== currentPhrase) {
-      // If the displayed text is not equal to the current phrase, update it letter by letter
+      
       const timeoutId = setTimeout(() => {
         setDisplayedText((prevText) => currentPhrase.substring(0, prevText.length + 1));
       }, 100);
   
-      // Clear the timeout when the component unmounts or when the phrase is fully displayed
+      
       return () => clearTimeout(timeoutId);
     } else {
-      // If the phrase is fully displayed, move on to the next one after a delay
       const timeoutId = setTimeout(() => {
         setCurrentPhraseIndex((prevIndex) => (prevIndex + 1) % phrases.length);
-        setDisplayedText(''); // Reset displayedText when moving to the next phrase
+        setDisplayedText(''); 
       }, 1000);
   
-      // Clear the timeout when the component unmounts or when moving to the next phrase
+      
       return () => clearTimeout(timeoutId);
     }
   }, [displayedText, currentPhraseIndex]);
@@ -38,7 +37,30 @@ export default function Body () {
             <p className="py-6">
              A passionate and results-driven Web Developer. My journey has equipped me with a diverse skill set, combining technical expertise with a creative approach to web developemnet.
             </p>
+            <div className='flex justify-center gap-4'>
+
             <button className="btn btn-primary"><a href='#project_cards' >Go To Projects</a></button>
+
+            <a href="https://github.com/asakshat"
+      ><button className="btn btn-primary">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 "
+          viewBox="0 0 16 16"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+          ></path>
+        </svg>
+         Github
+      </button></a
+    >
+            </div>
+        
+
+
             <div className="p-10">
             <kbd className="kbd">npm</kbd>
             
